ff07f6824b45b1cc14641ca8e9a5d448c139dff6
carolinavc99 <48517052+carolinavc99@users.noreply.github.com>
2022-04-19 10:35:15 +0100

    alinea 4 half-done

--- TP7/routes/index.js
+++ TP7/routes/index.js
@@ -1,18 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var axios = require("axios");
-var apikey = "&apikey=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNGNiYTg0OWJhYmI2NjdjYmZkYzE2ZSIsImlhdCI6MTY0OTE5NTY1MiwiZXhwIjoxNjUxNzg3NjUyfQ.EuvH713Qr6IZ073-5FMF6j5p_3tb6Trv0TOOF5ZHWOPUlCBqKU1H9DTo_ueoCyWhPbEd6F8xzNvn-UkG3J8Ppq65xF8uukoElnSIsi3kldXI2E_EHMv5ETIq-2SGpiBmLyv1zu2broi-nXw18XwKM-WWpoumw5mZacg1qyj4kokGm--WzPIDD15Uibu2ObsDfeHpbDt81Npq-WgEVe56F5w0TdAvY_b-Xvm77hXI4MuaatL9bsOtYEyiepLuBelDyVWjAIoon3-7tB1lwrPnC0OJ_cxKUyCdqx8sZPkmciyTmBsV8fDTyvTP1ibiryAQsDRK5TrG83CcWmStZyDnoQ"
+var apikey = "apikey=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNGNiYTg0OWJhYmI2NjdjYmZkYzE2ZSIsImlhdCI6MTY0OTE5NTY1MiwiZXhwIjoxNjUxNzg3NjUyfQ.EuvH713Qr6IZ073-5FMF6j5p_3tb6Trv0TOOF5ZHWOPUlCBqKU1H9DTo_ueoCyWhPbEd6F8xzNvn-UkG3J8Ppq65xF8uukoElnSIsi3kldXI2E_EHMv5ETIq-2SGpiBmLyv1zu2broi-nXw18XwKM-WWpoumw5mZacg1qyj4kokGm--WzPIDD15Uibu2ObsDfeHpbDt81Npq-WgEVe56F5w0TdAvY_b-Xvm77hXI4MuaatL9bsOtYEyiepLuBelDyVWjAIoon3-7tB1lwrPnC0OJ_cxKUyCdqx8sZPkmciyTmBsV8fDTyvTP1ibiryAQsDRK5TrG83CcWmStZyDnoQ"
 var date = new Date().toISOString().substring(0, 10)
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  axios.get('http://clav-api.di.uminho.pt/v2/classes?nivel=1' + apikey)
+  axios.get('http://clav-api.di.uminho.pt/v2/classes?nivel=1&' + apikey)
     .then(resp => {
       var dados = resp.data;
       res.render('index', { data: dados , title: "Index", date: date});
     })
     .catch(error => {
       res.render('error', { error: error });
+    });
+});
+
+router.get(/\/classes\/[0-9\.]+/, function(req, res) {
+  var id = req.url.split("/")[2]
+  console.log("Consulting class " + id)
+  axios.get('http://clav-api.di.uminho.pt/v2/classes/c' + id + "?" + apikey)
+    .then(resp => {
+      var dados = resp.data;
+      res.render('classe', { data: dados , title: "Classe", date: date});
+    })
+    .catch(error => {
+      res.render('error', { error: error, title: "Error" , message: "Error"});
     });
 });
 
